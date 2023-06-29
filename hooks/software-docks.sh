4030dcaeeb84305f2298b036c788c952e75021c2
jpacheco <jpacheco@barracuda.com>
2023-06-29 10:08:54 -0400

    Updating exit logic of software-docks

--- hooks/software-docks.sh
+++ hooks/software-docks.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -euo pipefail
 
-cat <<EOT > DOCS.md
+contents=$(cat <<EOT
+# Documentation
+
+Documentation detailing what software is installed on the built image.
+
 ## OS Information
 
 $(grep -w FROM Dockerfile | awk '{print tolower($0)}' | sed -r 's/(.*:)(.*)-(.*)$/- \2 \3/')
@@ -9,3 +14,23 @@ $(grep -w FROM Dockerfile | awk '{print tolower($0)}' | sed -r 's/(.*:)(.*)-(.*)
 
 $(grep -w "ARG .*_VERSION=\"[0-9]*\.[0-9]*\.[0-9]*\"" Dockerfile | awk '{print tolower($0)}' | sed -r 's/arg(.*)(_version=\")(.*)\"$/-\1 \3/')
 EOT
+)
+
+generate_docs() {
+    echo "$contents" > $1
+}
+
+if [ ! -f ./DOCS.md ]; then
+    echo "DOCS.md not found. Generating..."
+    generate_docs "DOCS.md"
+    exit 1
+fi
+
+if [ "x$contents" != "x$(cat DOCS.md)" ];then
+    echo "Updating DOCS.md"
+    generate_docs "DOCS.md"
+    exit 1
+fi
+
+echo "Docs are up to date"
+exit 0
